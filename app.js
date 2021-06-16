8467fe2a7990374acece35805376d02c7c9742ec
Sameer <sameer@Sameers-MacBook-Pro.local>
2021-06-16 17:15:40 +0530

    final step

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const p1button = document.querySelector('#p1button')
 const p2button = document.querySelector('#p2button')
 const p1dispaly = document.querySelector('#p1display')
 const p2dispaly = document.querySelector('#p2display')
-const reset=document.querySelector('#reset')
+const resetbtn = document.querySelector('#reset')
+const winningscoreselect = document.querySelector('#playto')
 
 let p1score = 0
 let p2score = 0
@@ -13,6 +14,8 @@ p1button.addEventListener('click', function () {
         p1score += 1
         if (p1score === winningscore) {
             isgameover = true
+            p1display.classlist.add('has-text-success')
+            p2display.classlist.add('has-text-danger')
         }
         p1display.textContent = p1score
     }
@@ -25,16 +28,29 @@ p2button.addEventListener('click', function () {
         p2score += 1
         if (p2score === winningscore) {
             isgameover = true
+            p2display.classlist.add('has-text-success')
+            p1display.classlist.add('has-text-danger')
         }
         p2display.textContent = p2score
     }
 
 })
 
-reset.addEventListener('click', function(){
-    isgameover=false
-    p1score=0
-    p2score=0
-    p1display.textContent=0
-    p2dispaly.textContent=0
+winningscoreselect.addEventListener('change', function () {
+    winningscore = parseInt(this.value)
+    reset()
 })
+
+resetbtn.addEventListener('click', reset)
+
+
+function reset() {
+    isgameover = false
+    p1score = 0
+    p2score = 0
+    p1display.textContent = 0
+    p2dispaly.textContent = 0
+    p1display.classlist.remove('has-text-success', 'has-text-danger')
+    p2display.classlist.remove('has-text-success', 'has-text-danger')
+}
+
