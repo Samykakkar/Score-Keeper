1ef88f88f51473fba9689194f44b3a481fed3728
Sameer <sameer@Sameers-MacBook-Pro.local>
2021-06-16 11:25:23 +0530

    Logic OK

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-const p1button= document.querySelector('#p1button')
-const p2button= document.querySelector('#p2button')
-const p1dispaly=document.querySelector('#p1display')
-const p2dispaly=document.querySelector('#p2display')
+const p1button = document.querySelector('#p1button')
+const p2button = document.querySelector('#p2button')
+const p1dispaly = document.querySelector('#p1display')
+const p2dispaly = document.querySelector('#p2display')
+const reset=document.querySelector('#reset')
 
-let p1score=0
-let p2score=0
-p1button.addEventListener('click', function(){
-    p1score+=1
-    p1display.textContent=p1score
+let p1score = 0
+let p2score = 0
+let winningscore = 5
+let isgameover = false
+p1button.addEventListener('click', function () {
+    if (!isgameover) {
+        p1score += 1
+        if (p1score === winningscore) {
+            isgameover = true
+        }
+        p1display.textContent = p1score
+    }
+
 })
 
 
-p2button.addEventListener('click', function(){
-    p2score+=1
-    p2display.textContent=p2score
+p2button.addEventListener('click', function () {
+    if (!isgameover) {
+        p2score += 1
+        if (p2score === winningscore) {
+            isgameover = true
+        }
+        p2display.textContent = p2score
+    }
+
+})
+
+reset.addEventListener('click', function(){
+    isgameover=false
+    p1score=0
+    p2score=0
+    p1display.textContent=0
+    p2dispaly.textContent=0
 })
